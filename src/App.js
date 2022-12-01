cea12be82c98e659bf3b3c386e4f2e8dada7f5f5
luizmosciaro <edu.mosciaro@live.com>
2022-12-01 19:42:47 -0400

    Aula 10 useState e hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import './index.css';
-import Evento from './components/Evento';
 import Form from './components/Form';
 
 function App() {
@@ -8,10 +7,7 @@ function App() {
     document.title = 'React App',
 
     <div className="App">
-      <h1>Aula sobre Evento</h1>
-      <Evento numero={1}/>
-      <Evento numero={2}/>
-      <Evento numero={3}/>
+      <h1>Aula de useState e hook</h1>
       <Form />
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,46 @@
+import {useState} from 'react'
+
 function Form() {
     
     function cadastrarUsuario(event){
         event.preventDefault()
-        console.log(`Usuario cadastrado: ${event.target[0].value}`)
+        console.log(`Usuario cadastrado: ${name} \nSenha: ${password}`)
     }
     
+    const [name, setName] = useState('Kabllez')
+    const [password, setPassword] = useState('12345')
+
     return (
         <>
             <h1>Cadastro de usuario:</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type='text' placeholder='Seu Nome'></input>
+                    <label htmlFor='name'>Nome:</label>
+                    <input 
+                    type='text' 
+                    placeholder='Seu Nome' 
+                    id='name' 
+                    name='name' 
+                    value = {name}
+                    onChange={(e)=> setName(e.target.value)}></input>
                 </div>
                 <div>
-                    <input type='submit' value='Cadastrar'></input>
+                    <label 
+                    htmlFor='password'>Senha:</label>
+                    <input 
+                    type='text' 
+                    placeholder='Senha' 
+                    id='password' 
+                    name='password'
+                    value={password}
+                    onChange={(e)=> setPassword(e.target.value)}></input>
                 </div>
+                    <input 
+                    type='submit' 
+                    placeholder='cadastrar' 
+                    id='cadastrar' 
+                    name='cadastrar'
+                    ></input>
             </form>
         </>
     )
