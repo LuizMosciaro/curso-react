6f15b9314a253a5e3f88fbf59d884bf5dd516211
luizmosciaro <edu.mosciaro@live.com>
2022-12-03 21:14:50 -0400

    React Icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,16 @@
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+import styles from './pages/layout/Footer.module.css'
+
 function Footer(){
-    return <footer>Rodape footer</footer>
+    return (
+        <footer>
+            <ul className={styles.social_list}>
+                <li> <FaFacebook/></li>
+                <li> <FaInstagram/></li>
+                <li> <FaLinkedin/></li>
+            </ul>
+            <p>Rodape da pagina</p>
+        </footer>    
+    )
 }
 export default Footer
